889bcab02aaadd9a735ce7108e6577c564367b90
rizaljabarudin <rizaljabarudin03@gmail.com>
2024-03-14 06:07:31 +0700

    first commit

--- tugas3_3.py
+++ tugas3_3.py
@@ -1,5 +1,5 @@
 jadwal_daspro_if3 = {'Senin': 'Jam 08:00', 'Kamis': 'Jam 10:50', 'Jumat': 'Jam 08:00'}
-jadwal_daspro_if2 = {'Selasa': 'Jam 08:00', 'Rabu': 'Jam 08:00'}
+jadwal_daspro_if2 = {'Selasa': 'Jam 08:00', 'Rabu': 'Jam 10:50', 'Rabu': 'Jam 15:00'}
 
 jadwal_daspro_if3.update(jadwal_daspro_if2)
 
