f83924216601b703528e720cc5ea226f316771f3
rizaljabarudin <rizaljabarudin03@gmail.com>
2024-03-20 13:31:04 +0700

    first commit

---
+++ tugas4.py
@@ -0,0 +1,31 @@
+masukan_bilangan = int ( input ("Masukan Bilangan : "))
+masukan_pilihan_user = int (input("Silahkan Pilih 1,2,3,4 : "))
+
+bil_oktal = oct(masukan_bilangan) [2:]
+bil_hex = hex(masukan_bilangan) [2:]
+bil_des = masukan_bilangan
+bil_bin = "{:08b}".format(masukan_bilangan)
+
+
+def masukan_pilihan (pilihan) :
+    if pilihan == 1 :
+        print("Bilangan Oktal Dari",masukan_bilangan,"Adalah",bil_oktal)
+    elif pilihan == 2 :
+        print("Bilangan Heksadesimal Dari",masukan_bilangan,"Adalah",bil_hex)
+    elif pilihan == 3 :
+        print("Bilangan Desimal Dari",masukan_bilangan,"Adalah",bil_des)
+    elif pilihan == 4 :
+        print("Bilangan Biner Dari",masukan_bilangan,"Adalah",bil_bin)
+    else :
+        print("Pilihan Tidak Sesuai")
+
+masukan_pilihan(masukan_pilihan_user)
+    
+
+
+
+
+# print("Bilangan biner:",masukan_bilangan,"adalah",bil_bin)
+# print("Bilangan oktal dari",masukan_bilangan,"adalah", bil_oktal)
+# print("Bilangan hexadesimal dari",masukan_bilangan,"adalah", bil_hexa)
+# print("Bilangan Desimal dari",masukan_bilangan,"adalah",bil_desimal)
