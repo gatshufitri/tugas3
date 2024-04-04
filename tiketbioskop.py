b21e996f855bc1b73feb9e15a3d26a5a00b9613b
rizaljabarudin <rizaljabarudin03@gmail.com>
2024-04-04 16:05:32 +0700

    first commit

--- tiketbioskop.py
+++ tiketbioskop.py
@@ -1,3 +1,9 @@
+kel4 = '''
+=============== Tiket Bioskop ==============
+============== Keloompok Empat =============
+'''
+print(kel4)
+
 listfilm = {
    'Action': {1: {'Judul': 'Lift', 'Waktu': '15:00 - 17:00 WIT'},
                2: {'Judul': 'Star Wars', 'Waktu': '13:00 - 15:00 WIT'},
@@ -29,16 +35,31 @@ listharga = {
              'Sabtu': '45.000',
              'Minggu': '50.000'}
 }
+listkel4 = {
+    'Nama':{'Muhamad Gatshu Fitri': '07352311183',
+            'M Thaariq Ramadhan': '07352211165',
+            'Mujaddid Muslim Mubarak Patty': '07352311161',
+            'Nabila Azahra Assagaf': '07352311163',
+            'Ningketaty Aryani Uropmabin': '07352211111',
+            'Serli Muhammad': '07352311175',
+            'Yusri Malagapi': '07352111021',
+            'Nurlini Ibrahim': '02352111016',
+            'Wahdania Afendi': '07352311177',
+            'Nabil Tamim Abdullah': '07352311162'}
+}
 listnama = []
 listumur = []
+listduduk = []
+listbaris = []
+listkolom = []
 
-print("====Daftar Genre Film====")
+print("====== Daftar Genre Film ======")
 for genre in listfilm:
     print(genre)
 
 pilih_genre = input("Pilih Genre Film : ")
 if pilih_genre in listfilm:
-    print(f"\n====Daftar Film {pilih_genre}====")
+    print(f"\n======= Daftar Film {pilih_genre} =======")
     for nomor, film in listfilm[pilih_genre].items():
         print(f"{nomor}. {film['Judul']}")
 
@@ -46,11 +67,10 @@ if pilih_genre in listfilm:
     if pilih_no_film in listfilm[pilih_genre]:
         judul_film_dipilih = listfilm[pilih_genre][pilih_no_film]['Judul']
         waktu_film_dipilih = listfilm[pilih_genre][pilih_no_film]['Waktu']
-    print(f"Waktu Film {judul_film_dipilih}: {waktu_film_dipilih}")
+        print(f"Waktu Film {judul_film_dipilih}: {waktu_film_dipilih}")    
 
-
-    print('''\n========Silahkan Pilih Hari=========
-=======Untuk Menentukan Harga=======''')
+    print('''\n======== Silahkan Pilih Hari =========
+======= Untuk Menentukan Harga =======''')
     for hari, harga in listharga['Hari'].items():
         print(f"{hari}")
 
@@ -61,30 +81,99 @@ if pilih_genre in listfilm:
             confrim = input("\nApakah Anda Ingin Membeli Tiket ? [Y/T] : ")
             if confrim == 'Y' :
                 jumlah_tiket = int(input("Masukan Jumlah Tiket : "))
-            
+                
                 for tiket in range (jumlah_tiket):
-                    masukan_nama = (input(f"Masukan Nama Orang Ke -{tiket+1}: "))
-                    masukan_umur = int(input("Masukan Umur Anda : "))
-                    listnama.append (masukan_nama)
-                    listumur.append (masukan_umur)
-
-                for x in range (masukan_umur):
-                    if masukan_umur >=18 :
-                        print("Anda Diperbolehkan Nonton")                        
+                        masukan_nama = (input(f"Masukan Nama Orang Ke -{tiket+1}: "))
+                        masukan_umur = int(input("Masukan Umur Anda : "))
+                        listnama.append (masukan_nama)
+                        listumur.append (masukan_umur)
+                        for x in range (masukan_umur):
+                            if masukan_umur >=18 :
+                                print("Anda Diperbolehkan Nonton")                        
+                            else :
+                                print("Anda Tidak Diperbolehkan Nonton")
+                            break
+                   
+            def peta_bioskop (baris,kolom):
+                for duduk in range  (baris):
+                    listduduk.append(['0']*kolom)
+                return listduduk
+            def tampilkan_peta(listduduk):
+                for baris in listduduk:
+                    print(' '.join(baris))
+            def pilih_tempat_duduk(listduduk):
+                for tempat in range (jumlah_tiket):
+                    masukan_baris = int(input(f"Masukan Nomer Baris Orang Ke-{tempat+1}: "))
+                    masukan_kolom = int(input(f"Masukan Nomer Kolom orang Ke-{tempat+1}: "))
+                    listbaris.append (masukan_baris)
+                    listkolom.append (masukan_kolom)
+                    if listduduk[masukan_baris - 1][masukan_kolom - 1] == 'X':
+                        print("Tempat Duduk Tersebut Sudah Teerisi.")
                     else :
-                        print("Anda Tidak Diperbolehkan Nonton")
-                    break  
+                        listduduk[masukan_baris - 1][masukan_kolom - 1] = 'X'
+                        print("Tempat Duduk Berhasil Dipilih")
+            break        
+        buat_peta_bioskop = peta_bioskop(5,5)
+    
+        print("\n==== Peta Tempat Duduk Bioskop ====")
+        tampilkan_peta(buat_peta_bioskop)
+        pilih_tempat_duduk(buat_peta_bioskop)
+        print("\nPeta Tempat Duduk Bioskop Setelah Dipilih : ")
+        tampilkan_peta(buat_peta_bioskop)   
+               
+        
+        import sys
+        while True :
+            print("\n======== Menu ========")
+            print("1. Lihat Nama Kelompok")
+            print("2. Informasi Tiket")
+            print("3. Exit")
+            masukan_pilihan = input("Pilih Menu (1-3) : ")
+            if masukan_pilihan == "1":
+                print("\n============ Kelompok 4 ============")
+                for nama,npm in listkel4 ['Nama'].items():
+                    print(f"Nama : {nama}")
+                    print(f"NPM  : {npm}")
+                    print("====================================")
+                    
+                while True :
+                    import sys
+                    print("1. Exit")
+                    masukan_pilihan = input("Pilih Menu (1) : ")
+                    if masukan_pilihan == "1":
+                        print("Terimakasih Sudah Berkunjung")
+                        sys.exit()
+                    else:
+                        print("Pilihan Tidak Valid")
+                            
+            elif masukan_pilihan == "2":
                 for info in range (jumlah_tiket):
-                    print("\n====Informasi Tiket====")
-                    print("Nama Anda    : ",listnama[info])
-                    print("Umur Anda    : ",listumur[info])
-                    print("Film         : ",judul_film_dipilih)
-                    print("Waktu Film   : ",pilih_hari,waktu_film_dipilih)
-                    print("Jumlah Tiket : ",jumlah_tiket)
-                    print("Terimakasih",listnama[info],"Telah Memesan Tiket",judul_film_dipilih)
-            break
-                       
+                         print("\n============== Informasi Tiket ==============")
+                         print("Nama Anda    : ",listnama[info])
+                         print("Umur Anda    : ",listumur[info])
+                         print("Film         : ",judul_film_dipilih)
+                         print("Waktu Film   : ",pilih_hari,waktu_film_dipilih)
+                         print("Jumlah Tiket : ",jumlah_tiket)
+                         print("Tempat Duduk : ","Baris",listbaris[info],"Kolom",listkolom[info])
+                         tampilkan_peta(buat_peta_bioskop)
+                         print("Terimakasih",listnama[info],"Telah Memesan Tiket",judul_film_dipilih)
+                         print("=============================================")
+                while True :
+                    import sys
+                    print("1. Exit")
+                    masukan_pilihan = input("Pilih Menu (1) : ") 
+                    if masukan_pilihan == "1":
+                        print("Terimakasih Sudah Berkunjung")
+                        sys.exit()
+                    else :
+                         print("Pilihan Tidak Valid")                            
+                
+            elif masukan_pilihan == "3":
+                 print("Terimakasih Sudah Berkunjung")
+                 sys.exit()
+            else :
+                print("Pilihan Tidak Valid")                               
     else:
-        print("Hari tidak valid.")
+        print("Hari tidak valid Silahkan Refresh.")
 else:
-    print("Pilihan Tidak Valid")
+    print("Pilihan Tidak Valid Silahkan Refresh")
